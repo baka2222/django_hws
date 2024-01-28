d64a688f1ad3f04ccc29d92bf44f712a33731698
baka222 <ikaoss222@gmail.com>
2024-01-29 04:51:12 +0600

    hw4

--- tv_show/urls.py
+++ tv_show/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import movies_list, movies_detail, create_post
 
 urlpatterns = [
-    path('movies/', movies_list),
+    path('', movies_list),
     path('movies/<int:id>', movies_detail),
     path('movies/add_post/', create_post, name='create_post')
 ]
